0a03067f515938a80a03e298a5c2cc1818d18e27
Dh <g.darigo01@ufromail.cl>
2022-04-24 23:50:22 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-null
+//document.getElementById("demo").innerHTML = "https://gadh210.github.io/";
+ 
+fetchText(); 
+
+async function fetchText() {
+    let response = await fetch("https://www.windfinder.com/forecast/freire_araucania_chile");
+
+    console.log(response.status); // 200
+    console.log(response.statusText); // OK
+
+    if (response.status === 200) {
+        let data = await response.text();
+        // handle data
+        document.getElementById("Title").innerHTML = data.substring(
+              str.indexOf('<span id="spotheader-spotname" class="spot-headline__large">') + 1,
+              str.lastIndexOf('</span>'));
+    } else {
+    	document.getElementById("Title").innerHTML = "404";
+    }
+}
