fa5dfadb8e187a71056b7709fa7d6cd7fa42195b
Dh <g.darigo01@ufromail.cl>
2022-04-25 00:46:03 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ async function fetchText() {
     console.log(response.status); // 200
     console.log(response.statusText); // OK
 
-    if (response.status == 200) {
+    if (response.status === 200) {
         let data = await response.text();
         // handle data
         document.getElementById("title").innerHTML = data.substring(
